53caa646141575148a550068521938cbca5e6ce7
saera1988 <saera1988@hotmail.com>
2019-10-22 17:18:14 +0300

    lab

--- examples/main.js
+++ examples/main.js
@@ -1,5 +1,23 @@
 // We loaded axios library in our html file
 // Lets make an ajax call with axios
-
-// method: get
-// url: https://cataas.com/cat
+const img = document.createElement('img');
+const button = document.createElement('button');
+const apiUrl = `https://pokeapi.co/api/v2/pokemon/1/`;
+button.innerText = "Random";
+document.body.append(button);
+const getRandomCat = () => {
+    axios({
+        url: apiUrl,
+        method: 'get',
+    }
+    )
+        .then(res => {
+            console.log(res)
+            img.src=res.data.sprites.front_shiny;
+            document.body.appendChild(img)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+button.addEventListener('click', getRandomCat);
